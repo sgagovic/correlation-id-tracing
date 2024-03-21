75ed34de0f6d0f3ef73cc8064aaddc4881195380
Sanja Gagovic <sanja.gagovic@embriq.no>
2024-03-21 12:06:55 +0100

    added service for employee operations

--- src/main/java/service/EmployeeService.java
+++ src/main/java/service/EmployeeService.java
@@ -1,4 +1,20 @@
 package service;
 
+import model.Employee;
+
+import java.util.List;
+
 public interface EmployeeService {
+
+    List<Employee> getEmployees();
+
+    Employee addEmployee(Employee employee);
+
+    Employee getEmployeeById(Long id);
+
+    Employee updateEmployee(Long id, Employee employee);
+
+    Employee deleteEmployee(Long id);
+
+    String getEmployeeName(Long id);
 }
---
+++ src/main/java/service/EmployeeServiceImpl.java
@@ -0,0 +1,37 @@
+package service;
+
+import model.Employee;
+
+import java.util.List;
+
+public class EmployeeServiceImpl implements EmployeeService{
+    @Override
+    public List<Employee> getEmployees() {
+        return null;
+    }
+
+    @Override
+    public Employee addEmployee(Employee employee) {
+        return null;
+    }
+
+    @Override
+    public Employee getEmployeeById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Employee updateEmployee(Long id, Employee employee) {
+        return null;
+    }
+
+    @Override
+    public Employee deleteEmployee(Long id) {
+        return null;
+    }
+
+    @Override
+    public String getEmployeeName(Long id) {
+        return null;
+    }
+}
